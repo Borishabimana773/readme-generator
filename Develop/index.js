26b360971943406b5573d79de801bbced873a473
Borishab <habimana40@gmail.com>
2022-11-13 14:13:52 -0500

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('markdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
