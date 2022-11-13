4b41317cd1396549858b2723a617f38579d99235
Borishab <habimana40@gmail.com>
2022-11-13 14:40:26 -0500

    Adding bried readme.me and installation

---
+++ Develop/dist/README.md
@@ -0,0 +1,36 @@
+# Professional Readme Generator
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+## Description
+An application that generates a readme file based on user inputs.
+
+## Table of Contents
+- [Professional Readme Generator](#professional-readme-generator)
+- [Description](#description)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributins](#contrinuting)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+npm i fs && i inquirer
+
+## Usage
+This application will allow user to save time and effort in writting a readme file. User needs to anser a few questions and the application will generate a professional readme file for them.
+
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+Application coved by MIT license
+
+## Contributing
+Boris
+
+## Tests
+None included
+
+## Questions
+Reachout to me on my github
+[Borishabimana773](https://github.com/Borishabimana773)
--- Develop/index.js
+++ Develop/index.js
@@ -5,13 +5,125 @@ const markdown = require('markdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the project title? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Write a brief description of your project: (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a brief description of your project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "installations steps: ",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter installation steps");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is this project usage for? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter what is this project usage for");
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Chose the appropriate license for this project: ",
+      choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+      default: "MIT",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Name of the contributor of this project? ",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Is there a test included? ",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "What do I do if I have an issue? (Required)",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Please enter your GitHub username: (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email: (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your email!");
+          return false;
+        }
+      },
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, markdown(date), (err) =>{
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('File Created!')
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions).then(data => writeToFile("./dist/README.md",data));
+}
 
 // Function call to initialize app
 init();
